2234279b81e3d5c20e4363143faf6923a18f5f49
Unknown <anushekprasal@gmail.com>
2018-05-08 01:13:28 +0530

    Init SixROM

--- qcom/sepolicy.mk
+++ qcom/sepolicy.mk
@@ -1,11 +1,11 @@
 #
 # This policy configuration will be used by all qcom products
-# that inherit from Lineage
+# that inherit from Six
 #
 
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
-    device/lineage/sepolicy/qcom/private
+    device/six/sepolicy/qcom/private
 
 BOARD_SEPOLICY_DIRS += \
-    device/lineage/sepolicy/qcom/common \
-    device/lineage/sepolicy/qcom/$(TARGET_BOARD_PLATFORM)
+    device/six/sepolicy/qcom/common \
+    device/six/sepolicy/qcom/$(TARGET_BOARD_PLATFORM)
